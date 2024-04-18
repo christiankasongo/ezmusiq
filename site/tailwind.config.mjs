bec2862b37c848355611d563c0e0c600b2f382c2
christiankasongo <45455847+christiankasongo@users.noreply.github.com>
2024-04-18 12:29:49 +0100

    Header
    Navbar links create, now working on website theme

--- site/tailwind.config.mjs
+++ site/tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
