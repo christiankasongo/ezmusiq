e89c12876d4880c63d5f1b9ee31697dee791a19d
christiankasongo <45455847+christiankasongo@users.noreply.github.com>
2024-05-13 10:37:10 +0100

    Header
    Header added

--- site/tailwind.config.mjs
+++ site/tailwind.config.mjs
@@ -2,11 +2,27 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     container: {
       padding: "2rem",
       center: true,
     },
-    extend: {},
+    extend: {
+      colors: {},
+      textColor: {
+        primary: "#444444",
+      },
+      fontFamily: {
+        primary: ["Heebo", "sans - serif"],
+        secondary: ["Signika", "sans - serif"],
+      },
+    },
   },
   plugins: [],
 };
